cee24ae8e6ae9e0ae3334ed0d6c3874ab418e057
Michael Nowak <michael.nowak@solidigmtechnology.com>
2024-06-07 12:47:09 -0700

    feat: add auth

--- auth.py
+++ auth.py
@@ -1,14 +1,53 @@
-class AuthenticationDummy:
-    def __init__(self):
-        self.user = None
-    
-    def login(self):
-        self.user = {
-            'name': 'John Smith',
-            'mail': 'John.Smith@solidigmtechnology.com',
-            }
-        
-    def logout(self):
-        self.user = None
+import streamlit as st
+from streamlit_oauth import OAuth2Component
+import os
 
-auth = AuthenticationDummy()
+from dotenv import load_dotenv
+load_dotenv()
+
+# Set environment variables
+AUTHORIZE_URL = os.environ.get('AUTHORIZE_URL')
+TOKEN_URL = os.environ.get('TOKEN_URL')
+REFRESH_TOKEN_URL = os.environ.get('REFRESH_TOKEN_URL')
+REVOKE_TOKEN_URL = os.environ.get('REVOKE_TOKEN_URL')
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+REDIRECT_URI = os.environ.get('REDIRECT_URI')
+SCOPE = os.environ.get('SCOPE')
+
+if any(envvar is None for envvar in [
+    AUTHORIZE_URL,
+    TOKEN_URL,
+    REFRESH_TOKEN_URL,
+    REVOKE_TOKEN_URL,
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI,
+    SCOPE,
+]):
+    raise Exception(f'Unset env var')
+
+
+def show_auth():
+    # Create OAuth2Component instance
+    oauth2 = OAuth2Component(CLIENT_ID, CLIENT_SECRET, AUTHORIZE_URL, TOKEN_URL, REFRESH_TOKEN_URL, REVOKE_TOKEN_URL)
+
+    # Check if token exists in session state
+    if 'token' not in st.session_state:
+        # If not, show authorize button
+        result = oauth2.authorize_button("Sign in", REDIRECT_URI, SCOPE)
+        if result and 'token' in result:
+            # If authorization successful, save token in session state
+            st.session_state.token = result.get('token')
+            for k, v in result.items():
+                print(f'{k}: {v}')
+            st.rerun()
+    else:
+        # If token exists in session state, show the token
+        token = st.session_state['token']
+        st.json(token)
+        if st.button("Refresh Token"):
+            # If refresh token button is clicked, refresh the token
+            token = oauth2.refresh_token(token)
+            st.session_state.token = token
+            st.rerun()
--- dashboard.py
+++ dashboard.py
@@ -4,20 +4,18 @@ Example dashboard with Okta authentication.
 
 import streamlit as st
 import pandas as pd
+from auth import show_auth
 
-from auth import auth
+from dotenv import load_dotenv
+load_dotenv()
 
-
-if not auth.user:
+if 'token' not in st.session_state:
     st.header('Sign in')
-    st.text('Please sign in to view this dashboard.')
-    st.button('Sign in', on_click=auth.login)
+    st.text('Please sign in to protect this very important data.')
 
-if auth.user:
-    st.header('Sign out')
-    st.text('Please sign out when you are done.')
-    st.button('Sign out', on_click=auth.logout)
+show_auth()
 
+if 'token' in st.session_state:
     st.header('Top Secret Data')
     st.text('It is crucial that this data remains secret.')
     st.text('Thank you for protecting out top secret bean data by not screenshotting this page.')
